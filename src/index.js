fe9015ea40c6f6b063ee871d97ef8ae36d4f6fb9
Naymco <naymco2013@gmail.com>
2019-09-02 17:40:06 +0200

    version 2.0

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // Settings:
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
